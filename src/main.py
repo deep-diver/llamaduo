f14d79a4cab143d1ae5f1a36459b81d2e9a8391a
Chansung Park <deep.diver.csp@gmail.com>
2024-03-25 03:26:29 +0900

    branch on valid condition

--- src/main.py
+++ src/main.py
@@ -4,26 +4,26 @@ from pipeline.eval import eval_on_records
 from utils import get_steps_right
 
 def main(args):
-    input_steps = get_steps_right(args.steps)
-    if "ft" in input_steps:
-        pass
-    
-    if "eval" in input_steps:
-        qualification, (avg_similarity_scores, avg_precision_scores) = eval_on_records(
-            args.ft_model_id, 
-            args.test_ds_id, args.test_ds_split, 
-            args.ft_model_config_path, args.prompt_tmpl_path,
-            args.avg_similarity_threshold, args.avg_precision_threshold,
-            args.gemini_api_key
-        )
-        print(f"qualification: {qualification}, avg_similarity_scores: {avg_similarity_scores}, avg_precision_scores: {avg_precision_scores}")
-        pass
-    
-    if "synth-gen" in input_steps:
-        pass
-    
-    if "deploy" in input_steps:
-        pass
+    valid, input_steps = get_steps_right(args.steps)
+    if valid:
+        if "ft" in input_steps:
+            pass
+        
+        if "eval" in input_steps:
+            qualification, (avg_similarity_scores, avg_precision_scores) = eval_on_records(
+                args.ft_model_id, 
+                args.test_ds_id, args.test_ds_split, 
+                args.ft_model_config_path, args.prompt_tmpl_path,
+                args.avg_similarity_threshold, args.avg_precision_threshold,
+                args.gemini_api_key
+            )
+            print(f"qualification: {qualification}, avg_similarity_scores: {avg_similarity_scores}, avg_precision_scores: {avg_precision_scores}")
+        
+        if "synth-gen" in input_steps:
+            pass
+        
+        if "deploy" in input_steps:
+            pass
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="LLMOps pipeline CLI")
