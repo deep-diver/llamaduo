b1996ad6a87c6d28bb943f2e864ae0ae9ce19213
chansung <deep.diver.csp@gmail.com>
2024-04-01 09:41:09 +0900

    only append if there is valid split

--- src/pipeline/utils.py
+++ src/pipeline/utils.py
@@ -20,16 +20,12 @@ def get_args(yaml_file_path):
     
     return configs
 
-def push_to_hf_hub(dataset_id, ds, hf_token, append=True):
-    exist = False
-
-    try:
-        create_repo(dataset_id, repo_type="dataset", token=hf_token)
-    except HfHubHTTPError as e:
-        exist = True
+def push_to_hf_hub(dataset_id, split, ds, hf_token, append=True):
+    create_repo(dataset_id, repo_type="dataset", exist_ok=True, token=hf_token)
     
-    if exist and append:
+    if append:
         existing_ds = load_dataset(dataset_id)
-        ds = concatenate_datasets([existing_ds['train'], ds])
+        if split in existing_ds.keys():
+            ds = concatenate_datasets([existing_ds[split], ds])
 
     ds.push_to_hub(dataset_id, token=hf_token)
